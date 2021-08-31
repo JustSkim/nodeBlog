9cde6267df10b7f7795e42ee9027deaecd98a48c
JustSkim <1424266960@qq.com>
2021-08-31 21:14:49 +0800

    对数据库的操作

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const fs = require('fs')
+const mysql = require('mysql')
 const util = require('./util.js')
 
 const app = express()
@@ -19,6 +19,41 @@ app.get('/',(req,resp)=>{
 app.get('/toLogin',async (req,resp)=>{
     const data = await util.read('pages/login.html')
     resp.end(data);
+})
+app.get('/getUser',(req,resp)=>{
+    const pool = mysql.createPool({
+        //连接我们本地名为'blog'的数据库，输入user和password
+        database:'blog',
+        user:'root',
+        password: ''
+    });
+    /*
+数据库连接池负责分配、管理和释放数据库连接，它允许应用程序重复使用一个现有的数据库连接，而不是再重新建立一个；释放空闲时间超过最大空闲时间的数据库连接来避免因为没有释放数据库连接而引起的数据库连接遗漏。这项技术能明显提高对数据库操作的性能。
+
+    用 createConnection 创建 Mysql 连接，每执行一次 connection.query 都是一个全新的连接，会造成一个资源的极大浪费，降低性能。
+
+连接池是另外的一种执行方法，它一次性的创建了多个连接，然后根据客户端的查询，自动的 分发、复用、管理 这些连接。
+
+我们首先用mysql.createPool或mysql.createConnection建立一个连接池pool，然后使用pool.getConnection从创建的连接池中获取到一个我们需要的连接.
+使用回调函数的参数connection来查询数据库。最后使用connection.realease()方法释放数据库连接
+When connecting to other servers, you will need to provide an object of options, in the same format as tls.createSecureContext. Please note the arguments expect a string of the certificate, not a file name to the certificate.
+    */
+    pool.getConnection((err,conn)=>{
+        if(!err){
+            const sql = 'select * from t_user'
+            const sqlParams = []
+            conn.query(sql,sqlParams,(e,results)=>{
+                if(!e){
+                    resp.send(results)
+                    console.log(results)
+                }
+            })
+        }
+        else{
+            console.log("err:",err)
+        }
+        conn.release();
+    })
 })
 
 app.listen(3000,()=>{
